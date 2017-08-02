d67ac53dcc4f12ba828a30852a52c8bb6db7dc1e
Robson Su <robsonsu00@gmail.com>
2017-08-01 21:04:40 -0300

    Initial commit

---
+++ app/src/main/java/com/example/kaua/businessgame/tela_login.java
@@ -0,0 +1,13 @@
+package com.example.kaua.businessgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tela_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_login);
+    }
+}
