a850a66af3aaba33e9cc7f9e2638a74bd5177f51
Robson Su <robsonsu00@gmail.com>
2017-09-23 18:29:16 -0300

    switch telas

--- app/src/main/java/com/example/kaua/businessgame/tela_tabuleiro.java
+++ app/src/main/java/com/example/kaua/businessgame/tela_tabuleiro.java
@@ -2,26 +2,14 @@ package com.example.kaua.businessgame;
 
 import android.content.Context;
 
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Point;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.Display;
-import android.view.View;
-import android.view.WindowManager;
 import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.View;
-
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -32,6 +20,7 @@ public class tela_tabuleiro extends AppCompatActivity {
     final static int maxN = 10;
     private Context context;
     private ImageView[][] ivCell = new ImageView[maxN][maxN];
+
     private Drawable[] drawCell = new Drawable[4];//0 is empty, 1 is player, 2 is bot, 3 is background
 
     private Button btnPlay;
@@ -45,8 +34,6 @@ public class tela_tabuleiro extends AppCompatActivity {
 
     }
 
-
-    Drawning v;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,7 +51,6 @@ public class tela_tabuleiro extends AppCompatActivity {
         drawCell[0] = null;//empty cell
 
     }
-    private boolean isClicked;//track player click cell or not => make sure that player only click to 1 cell
 
     @SuppressLint("NewApi")
     private void designBoardGame() {
@@ -83,32 +69,18 @@ public class tela_tabuleiro extends AppCompatActivity {
         for (int i = 0; i < maxN; i++) {
             LinearLayout linRow = new LinearLayout(context);
             //make a row
+
             for (int j = 0; j < maxN; j++) {
-                ivCell[i][j] = new ImageView(context);
-                //make a cell
-                //need to set background default for cell
-                //cell has 3 status, empty(defautl),player,bot
-                ivCell[i][j].setBackground(drawCell[3]);
-                final int x = i;
-                final int y = j;
-                //make that for safe and clear
-                ivCell[i][j].setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (valueCell[x][y] == 0) {//empty cell
-                            if (turnPlay == 1 || !isClicked) {//turn of player
-                                Log.d("tuanh","click to cell ");
-                                isClicked = true;
-                                xMove = x;
-                                yMove = y;//i,j must be final variable
-                                //     make_a_move();
-                            }
-                        }
-                    }
-                });
-                linRow.addView(ivCell[i][j], lpCell);
+                    ivCell[i][j] = new ImageView(context);
+                    //make a cell
+                    //need to set background default for cell
+                    //cell has 3 status, empty(defautl),player,bot
+                    ivCell[i][j].setBackground(drawCell[3]);
+                    linRow.addView(ivCell[i][j], lpCell);
+
             }
             linBoardGame.addView(linRow, lpRow);
+
         }
     }
 
